0fe2bc2ccac78be76156053244d76c664ab65ca0
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-09-29 14:21:02 +0200

    Create & GetAll

---
+++ Backend/Api/Infrastructure/Model/Box.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Model;
+
+public class Box
+{
+    public int ProductID { get; set; }
+    
+    [Required]
+    public string Title { get; set; }
+    
+    [Required]
+    public string Description { get; set; }
+    
+    [Required]
+    public decimal Price { get; set; }
+    
+    [Required]
+    public string ImageURL { get; set; }
+    
+    [Required]
+    public decimal Length { get; set; }
+    
+    [Required]
+    public decimal Width { get; set; }
+    
+    [Required]
+    public decimal Height { get; set; }
+}
---
+++ Backend/Api/Service/Service.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure;
+using Infrastructure.Model;
+
+namespace Service;
+
+public class Service
+{
+    private readonly Repository _repository;
+
+    public Service(Repository repository)
+    {
+        _repository = repository;
+    }
+
+    public Box CreateBox(string title, string description, decimal price, string imageUrl, decimal length,
+        decimal width, decimal height)
+    {
+        return _repository.CreateBox(title, description, price, imageUrl, length, width, height);
+    }
+
+    public IEnumerable<Box> GetAllProducts()
+    {
+        return _repository.GetAllProducts();
+    }
+}
