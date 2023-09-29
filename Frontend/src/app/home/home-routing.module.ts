9af353e935cabb55a6676da4fe961ef1c669ea04
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2023-09-29 12:59:02 +0200

    frontend skellet

--- Frontend/src/app/home/home-routing.module.ts
+++ Frontend/src/app/home/home-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import {ProductsComponent} from "../products/products.component";
+import {CreateBoxComponent} from "../create-box/create-box.component";
+import {DetailsComponent} from "../details/details.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage,
-  }
+    component: ProductsComponent,
+  },
+  {path: 'products', component: ProductsComponent},
+  {path: 'createBox', component: CreateBoxComponent},
+  {path: 'details', component: DetailsComponent},
+
 ];
 
 @NgModule({
--- Frontend/src/app/home/home.module.ts
+++ Frontend/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {ProductsComponent} from "../products/products.component";
 
 
 @NgModule({
@@ -14,6 +15,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  exports: [
+    HomePage,
+    ProductsComponent
+  ],
+  declarations: [HomePage, ProductsComponent]
 })
 export class HomePageModule {}
