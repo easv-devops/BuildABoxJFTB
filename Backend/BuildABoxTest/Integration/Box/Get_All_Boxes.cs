5a7d8c9cdde1f15fbffb670f5b64497a0b877dbc
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-10-03 13:05:23 +0200

    Test push med fejl test

--- Backend/BuildABoxTest/Integration/Box/Get_All_Boxes.cs
+++ Backend/BuildABoxTest/Integration/Box/Get_All_Boxes.cs
@@ -1,24 +1,78 @@
+using Dapper;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
 namespace BuildABoxTest.Integration.Box;
 
 public class Get_All_Boxes
 {
     /**
      * TO DO:
-     * 
+     *
      * 1. Instantier, send det ind, få objektet igen
      * 2. Get all uden der er instantieret noget
      * 3. Er f.eks. description blevet kortet ned (eller andre limits fra get all metoden)
-     * 
+     *
      */
+
+    private HttpClient _httpClient;
+    
     [SetUp]
     public void Setup()
     {
+        _httpClient = new HttpClient();
         Helper.TriggerRebuild();
     }
 
     [Test]
-    public void Dummy()
+    public async Task GetProductsFeed()
     {
-        
+        var expected = new List<Object>();
+        for (var i = 1; i < 10; i++)
+        {
+            var box = new Infrastructure.Model.Box()
+            {
+                ProductID = i,
+                Title = "Test box no. " + i,
+                Description = "Test description which is very very very very long " +
+                              "and exceeds the maximum amount of characters that we want to show.",
+                Price = 10,
+                ImageURL = "https://www.packable.sg/wp-content/uploads/2020/07/kraft_box_open.png",
+                Length = 25,
+                Width = 15,
+                Height = 10
+            };
+            expected.Add(box);
+            
+            var sql =
+                $@"
+            insert into buildabox.box (title, description, price, imageurl, width, length, height) 
+            values (@title, @description, @price, @imageUrl, @length, @width, @height) 
+            returning *;
+            ";
+            using (var conn = Helper.DataSource.OpenConnection())
+            {
+                conn.Execute(sql, box);
+            }
+
+            var url = "http://localhost:5273/products";
+            HttpResponseMessage response;
+            try
+            {
+                response = await _httpClient.GetAsync(url);
+                TestContext.WriteLine("The full body response: " 
+                                      + await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(Helper.NoResponseMessage, e);
+            }
+
+            using (new AssertionScope())
+            {
+                response.IsSuccessStatusCode.Should().BeFalse();
+
+            }
+        }
     }
 }
