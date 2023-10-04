7e52a86f2592e03404e0a6a94934e7d7e24a5f64
steffaneckeroth <stefeckeroth@hotmail.com>
2023-10-04 12:24:33 +0200

    xc

---
+++ Frontend/src/app/boxcard/boxcard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxcard',
+  templateUrl: './boxcard.component.html',
+  styleUrls: ['./boxcard.component.scss'],
+})
+export class BoxcardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
