8c05082ab396a09e4f425cc968d888cf36dc3c9b
steffaneckeroth <stefeckeroth@hotmail.com>
2023-10-10 12:06:28 +0200

    frontend search func

--- Frontend/src/app/products/products.component.ts
+++ Frontend/src/app/products/products.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {firstValueFrom} from "rxjs";
 import {Box} from "../boxcard/boxcard";
+import {environment} from "../../environments/environment";
 
 
 @Component({
@@ -25,4 +26,19 @@ export class ProductsComponent implements OnInit {
     const result = await firstValueFrom<Box[]>(call);
     this.boxes = result;
   }
+
+  async handleSearch(event: any) {
+
+    const emptyQuery = event.target.value === null ? '' : event.target.value;
+
+    const options = {
+      params : new HttpParams().set('searchQuery', emptyQuery)
+    }
+
+    const call = this.http.get<Box[]>(environment.apiBaseUrl + "/products/filter", options);
+    const result = await firstValueFrom<Box[]>(call);
+
+    this.boxes = result;
+  }
+
 }
